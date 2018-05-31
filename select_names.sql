d2535412747aa968c1258d75adc0cb5dc88037f8
Mohd Sohail Khan <mohdsohaildk@gmail.com>
2018-05-31 14:32:11 +0530

    solution added

--- select_names.sql
+++ select_names.sql
@@ -7,3 +7,58 @@ SELECT name FROM world
 --2.
 SELECT name FROM world
   WHERE name LIKE '%Y';
+  
+--3.
+SELECT name FROM world
+  WHERE name LIKE '%x%';
+  
+--4.
+SELECT name FROM world
+  WHERE name LIKE '%land';
+  
+--5.
+SELECT name FROM world
+  WHERE name LIKE 'C%ia';
+  
+--6.
+SELECT name FROM world
+  WHERE name LIKE '%oo%';
+  
+--7.
+SELECT name FROM world
+  WHERE name LIKE '%a%a%a%';
+  
+--8.
+SELECT name FROM world
+ WHERE name LIKE '_t%'
+ORDER BY name;
+
+--9.
+SELECT name FROM world
+ WHERE name LIKE '%o__o%';
+ 
+ --10.
+ SELECT name FROM world
+ WHERE name LIKE '____';
+ 
+ --11.
+ SELECT name
+  FROM world
+ WHERE name=capital;
+ 
+ --12.
+ SELECT name
+  FROM world
+ WHERE capital in (select capital from world where capital like '%city');
+ 
+ --13.
+ select capital, name from world where 
+capital like concat('%',name,'%');
+
+--14.
+select capital, name from world where 
+capital like concat(name,'%') and capital>name;
+
+--15.
+select name, replace(capital,name,'') from world where capital like concat('%',name,'%') and capital>name;
+
