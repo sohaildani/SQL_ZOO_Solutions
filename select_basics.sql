8269808c3562d949b659b90f09cd85f04f77a816
Mohd Sohail Khan <mohdsohaildk@gmail.com>
2018-05-31 12:09:38 +0530

    solution added

--- select_basics.sql
+++ select_basics.sql
@@ -1,1 +1,15 @@
 --- solution of select basics ---
+--1.
+SELECT population FROM world
+  WHERE name = 'germany';
+  
+  
+--2.
+SELECT name, population FROM world
+  WHERE name IN ('Sweden', 'Norway','Denmark');
+  
+--3.
+SELECT name, area FROM world
+  WHERE area BETWEEN 200000  AND 250000;
+  
+--4.
